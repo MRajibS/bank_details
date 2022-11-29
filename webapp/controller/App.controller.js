97c91c8f30ae4f30d852a0c2c6e3b6c623ce8881
Rajib Shaikh <rajib.shaikh@in.datacoresystems.com>
2022-11-29 05:25:21 +0000

    first commit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,6 +10,25 @@ sap.ui.define([
         return Controller.extend("com.sap.bankdetails.controller.App", {
             onInit: function () {
 
+            },
+
+            onOpenBankDetails: function () {
+                //create dialog lazily
+                if (!this.moreBankDetails) {
+                    this.moreBankDetails = this.loadFragment(
+                        {
+                            name: "com.sap.bankdetails.view.fragments.MoreDetails"
+                        }
+                    );
+                }
+                this.moreBankDetails.then(function (oDialog) {
+                    oDialog.open();
+                }
+                );
+            },
+            onCloseMoreDetails: function () {
+                this.byId("moreBankDetails").close();
             }
+
         });
     });
